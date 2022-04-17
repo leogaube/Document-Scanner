ac68557b98960c926fd2eb77b609ab73f8499634
Leo Gaube <gaubeleo@gmail.com>
2022-04-17 14:17:24 +0200

    edge + contour detection

--- scanner.py
+++ scanner.py
@@ -1,66 +1,65 @@
-import cv2
+import cv2 as cv
+import imutils
 import numpy as np
 
 import os
 
 
-def largest_rectangle(corner_canditates):
-    pass
+def find_document_corners(img):
+    # 1. preprocessing
+    # blur the image to reduce noise
+    blurred = cv.GaussianBlur(img, ksize=(5, 5), sigmaX=0)
 
+    # ToDo: more preprocessing
 
-def find_document_edges(img):
-    # preprocess
+    # 2. find contours
+    print("Edge Detection")
+    edge_img = cv.Canny(blurred, 75, 200)
+    print("Contour Detection")
+    contours, hierarchy = cv.findContours(edge_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-    # smoothing (gaussian blur?) (already part of canny/sobel?)
-    # canny vs sobel (computational expensive)
+    contour_img = img.copy()
+    cv.drawContours(contour_img, contours, -1, (0, 0, 255), 1)
 
-    # Harris corner detection
+    cv.imshow("Edges", edge_img)
+    cv.imshow("Contours", contour_img)
 
-    # hough transform --> find maxima --> find maxima
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = np.float32(gray)
-    corner_canditates = cv2.cornerHarris(gray, 2, 3, 0.04)
+    # 3. Find largest contour
 
-    corners = largest_rectangle(corner_canditates)
-
-    if DEBUG:
-        for corner in corners:
-            img[corner] = [0, 0, 255]
-
-        cv2.imshow("harris", img)
-        cv2.waitKey()
-
-    return corners
+    # 4. extract four corner points of largest contour
 
 
 def perspective_transform(img, edges):
     pass
 
 
+# Adaptive Thresholding?!
 def filter_binary(img):
     pass
 
 
 def scan(img, save=False):
-    edges = find_document_edges(img)
-    top_down_img = perspective_transform(img, edges)
+    doc_corners = find_document_corners(img)
+    top_down_img = perspective_transform(img, doc_corners)
     binary_document = filter_binary(top_down_img)
 
     if save:
         output_filename = os.path.join(".", "results", "document.jpg")
-        cv2.imwrite(output_filename, binary_document)
+        cv.imwrite(output_filename, binary_document)
 
     if DEBUG:
-        cv2.imshow("document", binary_document)
-        cv2.waitKey()
+        cv.imshow("document", binary_document)
+        cv.waitKey()
 
 
 if __name__ == "__main__":
-    DEBUG = True
+    DEBUG = False
 
-    filename = os.path.join(".", "test_imgs", "checkerboard.jpg")
-    test_img = cv2.imread(filename)
-    test_img = cv2.resize()
+    filename = os.path.join(".", "imgs", "checkerboard.jpg")
+    test_img = cv.imread(filename)
+    test_img = imutils.resize(test_img, width=360)
 
     scan(test_img)
